093592471a7905ef4d2fe5a51c5b0383b21ada25
Synnly <emanuelfernandes54@gmail.com>
2026-01-28 11:05:09 +0100

    Fix register

--- apps/client/src/hooks/register.ts
+++ apps/client/src/hooks/register.ts
@@ -2,7 +2,6 @@ import { useMutation } from '@tanstack/react-query';
 import { userStore } from '../stores/userStore';
 import { useNavigate } from 'react-router';
 import type { formSignUp } from '../modules/types/formRegister.type';
-import {UseAuthFetch} from "./authFetch.ts";
 
 export const UseRegister = () => {
     const setAccess = userStore((state) => state.set);
@@ -11,31 +10,54 @@ export const UseRegister = () => {
     const API_URL = import.meta.env.VITE_APIURL;
     if (!API_URL) throw new Error('API URL is not configured');
 
-    const { mutateAsync, isPending, isError, error, reset } = useMutation({
-        mutationFn: async (data: {
-            email: string;
-            password: string;
-            role: string;
-        }) => {
-            const authFetch = UseAuthFetch();
-            const res = await authFetch(`${API_URL}/user`, {
-                method: 'POST',
-                data: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            if (!res.ok) {
-                const message = await res.json();
-                throw new Error(message.message);
+    const performRegisterRequest = async (data: formSignUp): Promise<Response> => {
+        const dataToSend = { email: data.email, password: data.password, role: 'USER' };
+        let res = await fetch(`${API_URL}/user`, {
+            method: 'POST',
+            body: JSON.stringify(dataToSend),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        if (!res.ok) {
+            if (res.status === 419) {
+                const csrfRes = await fetch(`${API_URL}/csrf-token`, {
+                    method: 'GET',
+                    credentials: 'include',
+                });
+
+                if (!csrfRes.ok) {
+                    throw new Error('Erreur lors de la récupération du token CSRF');
+                }
+
+                res = await fetch(`${API_URL}/user`, {
+                    method: 'POST',
+                    body: JSON.stringify(dataToSend),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    credentials: 'include',
+                });
+                if (res.ok) {
+                    return res;
+                }
             }
-            return res;
-        },
+            const message = await res.json();
+            throw new Error(message.message);
+        }
+
+        return res;
+    }
+
+    const { mutateAsync, isPending, isError, error, reset } = useMutation({
+        mutationFn: performRegisterRequest,
     });
 
     const register = async (data: formSignUp) => {
         const { repeatPassword, ...registerData } = data;
-        const res = await mutateAsync({ ...registerData, role: 'USER' });
+        const res = await mutateAsync(data);
 
         if (res.ok) {
             const loginRes = await fetch(`${API_URL}/auth/login`, {
